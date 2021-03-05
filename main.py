45618b2ff2df622e43694a921f8b6db34fe7334a
Moein Aghamirzaei <moein.6556@gmail.com>
2021-03-05 03:57:29 +0330

    remove root_dir

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import sys
 from utils.initGui import GUI
-import sys, os
-
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 cur_path = sys.path[0]
-# cur_path = ROOT_DIR
 gui = GUI(cur_path)
 gui.start()
