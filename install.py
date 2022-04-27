9fac1fbbcc14e4226986d35417eb37e6317653a1
Moein Aghamirzaei <moein.6556@gmail.com>
2022-04-27 19:01:54 +0430

    fix installer

--- install.py
+++ install.py
@@ -1,17 +1,31 @@
-import os, sys
+import os
+import sys
 from pathlib import Path
 
 # os.system("sudo apt-get install -y python3-dev libasound2-dev")
 os.system("pip3 install -U pip wheel setuptools")
 os.system("sudo pip3 install -r requirements.txt")
 
-file = open('{}/.local/share/applications/moein-dns-changer.desktop'.format(Path.home()), 'w+')
 cur_path = sys.path[0]
+
+file = open(
+    '{}/.local/share/applications/moein-dns-changer.desktop'.format(Path.home()), 'w+')
 file.write('''[Desktop Entry]
 Name= DNS changer
 Comment= simple gui linux DNS changer with python3 and PyQt By Moein Aghamirzaei
-Exec= pkexec python3 {}/main.py
+Exec= dns_changer
 Icon= {}/icon/dns-logo.png
 Terminal=false
 Type=Application
 StartupNotify=true'''.format(cur_path, cur_path))
+file.close()
+
+file = open('{}/.local/bin/dns_changer'.format(Path.home()), 'w+')
+file.write('''
+#! /bin/bash
+exec pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY python3 {}/main.py
+'''.format(cur_path))
+file.close()
+os.system("sudo chmod +x {}/.local/bin/dns_changer".format(Path.home()))
+
+print('------------------\ninstalled successfully')
